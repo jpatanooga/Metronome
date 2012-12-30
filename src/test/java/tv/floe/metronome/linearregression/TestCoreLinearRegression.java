b99922f34519da5379b2bfa4ebd68a1c29f8bbf0
Josh Patterson <jpatterson@floe.tv>
2012-12-29 20:47:32 -0500

    linear regression continues to improve but needs a working bias term

--- src/test/java/tv/floe/metronome/linearregression/TestCoreLinearRegression.java
+++ src/test/java/tv/floe/metronome/linearregression/TestCoreLinearRegression.java
@@ -22,12 +22,19 @@ import com.google.common.collect.Multiset;
  * Run the sample data from the resources folder through the LR process - hand
  * check the resultant equation
  * 
+ * Current Issues: 
+ * - needs a bias term
+ * 
  * @author josh
  * 
  */
 public class TestCoreLinearRegression {
 
-	private static String file_name = "src/test/resources/linear_regression_sample_data.txt";
+	// lr: 0.003
+	//private static String file_name = "src/test/resources/linear_regression_sample_data.txt";
+	
+	// lr: 3
+	private static String file_name = "src/test/resources/sat_scores_svmlight.txt";
 	//private static String file_name = "src/test/resources/two_points.txt";
 	//private static String file_name = "src/test/resources/big_points.txt";
 	//private static String file_name = "src/test/resources/normalized_points.txt";
@@ -40,17 +47,17 @@ public class TestCoreLinearRegression {
 		ParallelOnlineLinearRegression polr = new ParallelOnlineLinearRegression(
 				2, new UniformPrior()).alpha(1)
 				.stepOffset(1000).decayExponent(0.9).lambda(3.0e-5)
-				.learningRate(.0003);
+				.learningRate(3);
 		
 		RCV1RecordFactory factory = new RCV1RecordFactory();
 
 		
-		for ( int x = 0; x < 10; x++ ) {
+		for ( int x = 0; x < 100; x++ ) {
 			
 			
 			BufferedReader reader = new BufferedReader(new FileReader(file_name));
 			double error_sum = 0;
-			
+			int rec_count = 0;
 			
 			String line = reader.readLine();
 			while (line != null && line.length() > 0) {
@@ -62,6 +69,8 @@ public class TestCoreLinearRegression {
 				if (null == line || line.trim().equals("")) {
 					
 				} else {
+					
+					rec_count++;
 	
 					Vector vec = new RandomAccessSparseVector(2);
 				    
@@ -72,35 +81,23 @@ public class TestCoreLinearRegression {
 				    // but here we only need a single parameter vector
 				    double hypothesis_value = polr.getBeta().viewRow(0).dot(vec);
 				    
-				//    System.out.println( "hyp: " + hypothesis_value );
-				    //System.out.println( "error: " + (hypothesis_value - actual) );
-				    
-				    
-				  //  System.out.print( "vec: ");
-				 //   Utils.PrintVector(vec);
-				    
 				    double error = SquaredErrorLossFunction.Calc(hypothesis_value, actual);
 				    error_sum += error;
 				    
-				    //System.out.println("> Err: " + error ); // + " \t#h: " + hypothesis_value + " \ta: " + actual );
-				    
-//				    System.out.println(">> Vec: " + vec.get(0) );
-	
-				    
-				    
 				    polr.train(actual, vec);
-				    
-				    //System.out.println("#\n");
-					
-//				    System.out.println(">> Beta: " + polr.getBeta().viewRow(0).get(0) );
-				    
+
+
+/*					String[] ar = line.split(" |f 0:");
+					//System.out.println(ar.length);
+					System.out.println( ar[2] + " |f 0:" + ar[0] );
+	*/			    
 				}
 				
 				line = reader.readLine();
 
 			} // while
 			
-			System.out.println("> " + x + " Avg Err: " + ( error_sum / (x+1) ) );
+			System.out.println("> " + x + " Avg Err: " + ( error_sum / ((x+1)*rec_count) ) );
 			
 			// reader.reset();
 			System.out.println("----------------------- ");			
