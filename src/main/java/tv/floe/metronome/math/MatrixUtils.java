077e68e021f3e83b89f2dccf91df00b3225199a2
Josh Patterson <jpatterson@floe.tv>
2013-12-20 22:31:07 -0500

    added a poisson distribution generator function on MatrixUtils

--- src/main/java/tv/floe/metronome/math/MatrixUtils.java
+++ src/main/java/tv/floe/metronome/math/MatrixUtils.java
@@ -242,7 +242,7 @@ public class MatrixUtils {
 		for (int r = 0; r < dist.numRows(); r++) {
 			for ( int c = 0; c < dist.numCols(); c++ ) {
 		
-				dist.set(r, c, binomial(rndNumberGenerator, max, pValues.get(r,  c) ) );
+				dist.set(r, c, getBinomial(rndNumberGenerator, max, pValues.get(r,  c) ) );
 				
 			}
 		}
@@ -263,7 +263,7 @@ public class MatrixUtils {
 	 * @param p
 	 * @return
 	 */
-	public static int binomial(RandomGenerator rng, int max, double p) {
+	public static int getBinomial(RandomGenerator rng, int max, double p) {
 		
 		if (p < 0 || p > 1) {
 			return 0;
@@ -284,6 +284,31 @@ public class MatrixUtils {
 		
 		return c;
 	}	
+	
+	/**
+	 * Generating numbers in a poisson distribution based on how Knuth says to do it:
+	 * 
+	 * http://en.wikipedia.org/wiki/Poisson_distribution#Generating_Poisson-distributed_random_variables
+	 * 
+	 * 
+	 * @param lambda
+	 * @return
+	 */
+	public static int getPoisson(double lambda) {
+		
+		double L = Math.exp(-lambda);
+		double p = 1.0;
+		int k = 0;
+		
+		do {
+			
+			k++;
+			p *= Math.random();
+			
+		} while (p > L);
+		
+		return k - 1;
+	}
 	
 	public void addRowVector(Matrix m, Vector row) {
 		
