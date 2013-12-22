a11df74b94120fb04f55752be2498db49deec602
Josh Patterson <jpatterson@floe.tv>
2013-12-21 22:08:29 -0500

    implemented equations (7) and (8) from Hinton's paper in RBM, needs unit test

--- src/main/java/tv/floe/metronome/deeplearning/rbm/RestrictedBoltzmannMachine.java
+++ src/main/java/tv/floe/metronome/deeplearning/rbm/RestrictedBoltzmannMachine.java
@@ -3,6 +3,8 @@ package tv.floe.metronome.deeplearning.rbm;
 import org.apache.commons.math3.random.RandomGenerator;
 import org.apache.mahout.math.Matrix;
 
+import tv.floe.metronome.math.MatrixUtils;
+
 
 
 /**
@@ -56,28 +58,35 @@ public class RestrictedBoltzmannMachine {
 		
 	}
 	
-	/*
-	 * 
-	public DoubleMatrix propUp(DoubleMatrix v) {
-		DoubleMatrix preSig = v.mmul(W);
-		preSig = preSig.addRowVector(hBias);
-		return MatrixUtil.sigmoid(preSig);
-
-	}
-
-	 * 
-	 */
-
 	/**
-	 * 
+	 * Generate probabilities for each hidden unit being set to 1
+	 * Equation (7) in Hinton
 	 * 
 	 * @param visible
 	 * @return
 	 */
-	public Matrix propUp(Matrix visible) {
+	public Matrix generateProbabilitiesForHiddenStatesBasedOnVisibleStates(Matrix visible) {
 		
 		Matrix preSigmoid = visible.times( this.connectionWeights );
-		
+		preSigmoid = MatrixUtils.addRowVector(preSigmoid, this.hiddenBiasNeurons.viewRow(0));
+
+		return MatrixUtils.sigmoid(preSigmoid);
 	}
+
+	/**
+	 * Generate probabilities for each visible unit being set to 1 given hidden states
+	 * Equation (8) in Hinton
+	 * 
+	 * @param visible
+	 * @return
+	 */
+	public Matrix generateProbabilitiesForVisibleStatesBasedOnHiddenStates(Matrix hidden) {
+		
+		Matrix preSigmoid = hidden.times( this.connectionWeights );
+		preSigmoid = MatrixUtils.addRowVector(preSigmoid, this.visibleBiasNeurons.viewRow(0));
+
+		return MatrixUtils.sigmoid(preSigmoid);
+	}
+	
 	
 }
---
+++ src/test/java/tv/floe/metronome/deeplearning/rbm/TestRestrictedBoltzmannMachine.java
@@ -0,0 +1,37 @@
+package tv.floe.metronome.deeplearning.rbm;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestRestrictedBoltzmannMachine {
+
+	/**
+	 * Tests functionality for the code based on equation (7) from Hinton
+	 * 
+	 */
+	@Test
+	public void testGenerateProbabilitiesForSettingHiddenStatesToOne() {
+		
+		
+		
+		
+	}
+
+	/**
+	 * Tests functionality for the code based on equation (8) from Hinton
+	 * 
+	 */
+	@Test
+	public void testGenerateProbabilitiesForSettingVisibleStatesToOne() {
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
