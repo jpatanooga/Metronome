a83d324607debcf64926eddb1bf18e00401d26f3
Josh Patterson <jpatterson@floe.tv>
2013-12-30 11:35:00 -0500

    WIP commits on vectorized NN stuff

--- src/main/java/tv/floe/metronome/deeplearning/neuralnetwork/core/LayerVectorized.java
+++ src/main/java/tv/floe/metronome/deeplearning/neuralnetwork/core/LayerVectorized.java
@@ -15,6 +15,18 @@ public class LayerVectorized {
 		
 	}
 	
+	/**
+	 * In the vectorized implementation we want to hook the previous layer's
+	 * work in progress into this layer
+	 * 
+	 * In the case of the first layer, its just the matrix of input samples
+	 * 
+	 * @param layerInput
+	 */
+	public void setInput(Matrix layerInput) {
+		
+	}
+	
 	
 	public Matrix getVectorizedOutputMatrix() {
 		
--- src/main/java/tv/floe/metronome/deeplearning/neuralnetwork/core/NeuralNetworkVectorized.java
+++ src/main/java/tv/floe/metronome/deeplearning/neuralnetwork/core/NeuralNetworkVectorized.java
@@ -5,6 +5,16 @@ package tv.floe.metronome.deeplearning.neuralnetwork.core;
  * Neural Network implementation (vectorization)
  * - designed to do work on all of the input samples at the same time via matrix ops
  * 
+ * This namespace is designed differently from the mainline NN OOP-implementation
+ * 
+ * 
+ * TODO
+ * - base api
+ * - get XOR training on this version
+ * - adagrad
+ * - L2 prior
+ * - momentum
+ * 
  * @author josh
  *
  */
