8fa85608094709de1e5480b55bbb2257a4cc41a8
Josh Patterson <jpatterson@floe.tv>
2013-12-29 22:45:59 -0500

    added base class for a vectorized nn

---
+++ src/main/java/tv/floe/metronome/deeplearning/neuralnetwork/NeuralNetworkVectorized.java
@@ -0,0 +1,13 @@
+package tv.floe.metronome.deeplearning.neuralnetwork;
+
+
+/**
+ * Neural Network implementation (vectorization)
+ * - designed to do work on all of the input samples at the same time via matrix ops
+ * 
+ * @author josh
+ *
+ */
+public class NeuralNetworkVectorized {
+
+}
