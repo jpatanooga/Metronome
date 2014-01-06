a4011917ea414bae3694d04af7583f35ea59c161
Josh Patterson <jpatterson@floe.tv>
2014-01-05 19:26:01 -0500

    stubbed out the DL-optimize namespace with some blank classes

---
+++ src/main/java/tv/floe/metronome/deeplearning/neuralnetwork/optimize/BackpropOptimization.java
@@ -0,0 +1,13 @@
+package tv.floe.metronome.deeplearning.neuralnetwork.optimize;
+
+public class BackpropOptimization {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
