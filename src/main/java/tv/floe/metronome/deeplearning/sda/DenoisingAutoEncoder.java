dc0efd75fd8c360a137447b575f8e7e16f26ec9b
Josh Patterson <jpatterson@floe.tv>
2014-01-08 21:57:33 -0500

    added some base unimplemented methods for Denoising AutoEncoder - you are welcome, Dave

--- src/main/java/tv/floe/metronome/deeplearning/sda/DenoisingAutoEncoder.java
+++ src/main/java/tv/floe/metronome/deeplearning/sda/DenoisingAutoEncoder.java
@@ -1,8 +1,28 @@
 package tv.floe.metronome.deeplearning.sda;
 
+import org.apache.mahout.math.Matrix;
+
 import tv.floe.metronome.deeplearning.neuralnetwork.core.BaseNeuralNetworkVectorized;
 
 public class DenoisingAutoEncoder extends BaseNeuralNetworkVectorized {
+
+	@Override
+	public Matrix reconstruct(Matrix x) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double lossFunction(Object[] params) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void train(Matrix input, double learningRate, Object[] params) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 	
