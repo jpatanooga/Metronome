548b4cb91ac82274556b8db37916e692b1381b4a
Josh Patterson <jpatterson@floe.tv>
2014-01-11 17:03:43 -0500

    added base test for deep belief networks for the UCI Iris dataset - Test_DBN_IrisDataset.java

---
+++ src/test/java/tv/floe/metronome/deeplearning/neuralnetwork/dbn/dataset/uci/iris/Test_DBN_IrisDataset.java
@@ -0,0 +1,41 @@
+package tv.floe.metronome.deeplearning.neuralnetwork.dbn.dataset.uci.iris;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import tv.floe.metronome.irunit.IRUnitDriver;
+
+public class Test_DBN_IrisDataset {
+
+	@Test
+	public void testLearnDatasetFunctionViaIR() throws Exception {
+		
+		/*
+		IRUnitDriver polr_ir = new IRUnitDriver("src/test/resources/run_profiles/unit_tests/nn/iris/app.unit_test.nn.iris.properties");
+		polr_ir.Setup();
+
+		polr_ir.SimulateRun();
+*/
+
+		
+		
+	}
+
+	
+	@Test
+	public void testLearnDatasetFunctionViaCoreLib() throws Exception {
+		
+		/*
+		IRUnitDriver polr_ir = new IRUnitDriver("src/test/resources/run_profiles/unit_tests/nn/iris/app.unit_test.nn.iris.properties");
+		polr_ir.Setup();
+
+		polr_ir.SimulateRun();
+*/
+
+		
+		
+	}
+	
+	
+}
