bcfba5e41ebb29a720c677da0b0a46f4374f5c10
Josh Patterson <jpatterson@floe.tv>
2014-01-24 22:56:05 -0500

    create class Sigmoid and gave a base impl

--- src/main/java/tv/floe/metronome/deeplearning/neuralnetwork/activation/Sigmoid.java
+++ src/main/java/tv/floe/metronome/deeplearning/neuralnetwork/activation/Sigmoid.java
@@ -1,5 +1,21 @@
 package tv.floe.metronome.deeplearning.neuralnetwork.activation;
 
-public class Sigmoid {
+import org.apache.mahout.math.Matrix;
+
+import tv.floe.metronome.math.MatrixUtils;
+
+
+public class Sigmoid implements ActivationFunction {
+
+	private static final long serialVersionUID = -6280602270833101092L;
+
+	public Matrix apply(Matrix arg0) {
+		return MatrixUtils.sigmoid(arg0);
+	}
+
+	@Override
+	public Matrix applyDerivative(Matrix input) {
+		return MatrixUtils.sigmoid( input ).times( MatrixUtils.oneMinus( MatrixUtils.sigmoid( input ) ) );
+	}
 
 }
