e0a533be365bddf668a151e3fd26dc7e709bfcce
Josh Patterson <jpatterson@floe.tv>
2014-02-01 19:13:21 -0500

    first draft of ModelTester for DBNs

---
+++ src/main/java/tv/floe/metronome/deeplearning/dbn/model/evaluation/ModelTester.java
@@ -0,0 +1,57 @@
+package tv.floe.metronome.deeplearning.dbn.model.evaluation;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.mahout.math.Matrix;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import tv.floe.metronome.berkley.Pair;
+import tv.floe.metronome.deeplearning.datasets.DataSet;
+import tv.floe.metronome.deeplearning.datasets.iterator.impl.MnistDataSetIterator;
+import tv.floe.metronome.deeplearning.neuralnetwork.core.BaseMultiLayerNeuralNetworkVectorized;
+import tv.floe.metronome.eval.Evaluation;
+
+
+public class ModelTester {
+	
+	
+	public void loadModel() {
+		
+		
+	}
+	
+
+	private static Logger log = LoggerFactory.getLogger(ModelTester.class);
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		
+		MnistDataSetIterator iter = new MnistDataSetIterator(10, 60000);
+		
+		Evaluation eval = new Evaluation();
+		BaseMultiLayerNeuralNetworkVectorized load = BaseMultiLayerNeuralNetworkVectorized.loadFromFile(new FileInputStream(new File(args[0])));
+		
+		while (iter.hasNext()) {
+			
+			DataSet inputs = iter.next();
+
+			Matrix in = inputs.getFirst();
+			Matrix outcomes = inputs.getSecond();
+			Matrix predicted = load.predict(in);
+			eval.eval( outcomes, predicted );
+			
+		}
+		
+		
+		
+		
+		log.info( eval.stats() );
+	}	
+	
+
+}
