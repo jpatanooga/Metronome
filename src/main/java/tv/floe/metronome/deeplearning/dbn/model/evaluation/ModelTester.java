a696ab9f62648e4bf4f8fa95299da969044c6af2
Josh Patterson <jpatterson@floe.tv>
2014-02-02 11:16:55 -0500

    added evaluateSavedModel() method to ModelTester for DBNs

--- src/main/java/tv/floe/metronome/deeplearning/dbn/model/evaluation/ModelTester.java
+++ src/main/java/tv/floe/metronome/deeplearning/dbn/model/evaluation/ModelTester.java
@@ -2,6 +2,7 @@ package tv.floe.metronome.deeplearning.dbn.model.evaluation;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.apache.mahout.math.Matrix;
@@ -10,6 +11,7 @@ import org.slf4j.LoggerFactory;
 
 import tv.floe.metronome.berkley.Pair;
 import tv.floe.metronome.deeplearning.datasets.DataSet;
+import tv.floe.metronome.deeplearning.datasets.iterator.BaseDatasetIterator;
 import tv.floe.metronome.deeplearning.datasets.iterator.impl.MnistDataSetIterator;
 import tv.floe.metronome.deeplearning.neuralnetwork.core.BaseMultiLayerNeuralNetworkVectorized;
 import tv.floe.metronome.eval.Evaluation;
@@ -18,10 +20,30 @@ import tv.floe.metronome.eval.Evaluation;
 public class ModelTester {
 	
 	
-	public void loadModel() {
+	public static void evaluateSavedModel( BaseDatasetIterator iterator, String modelLocation ) throws FileNotFoundException {
 		
+
+		Evaluation eval = new Evaluation();
+		BaseMultiLayerNeuralNetworkVectorized load = BaseMultiLayerNeuralNetworkVectorized.loadFromFile(new FileInputStream(new File(modelLocation)));
+		
+		while (iterator.hasNext()) {
+			
+			DataSet inputs = iterator.next();
+
+			Matrix in = inputs.getFirst();
+			Matrix outcomes = inputs.getSecond();
+			Matrix predicted = load.predict(in);
+			eval.eval( outcomes, predicted );
+			
+		}
+		
+		
+		
+		
+		log.info( eval.stats() );		
 		
 	}
+	
 	
 
 	private static Logger log = LoggerFactory.getLogger(ModelTester.class);
