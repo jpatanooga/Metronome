9f480253c61170ac0ceca61c694f1649dd8e3f67
Josh Patterson <jpatterson@floe.tv>
2014-02-05 23:06:10 -0500

    added base empty class for the RBM network visualizer - WIP

---
+++ src/main/java/tv/floe/metronome/deeplearning/rbm/visualization/NeuralNetworkVisualizer.java
@@ -0,0 +1,17 @@
+package tv.floe.metronome.deeplearning.rbm.visualization;
+
+
+/**
+ * Based on the designs in the paper:
+ * 
+ * http://yosinski.com/media/papers/Yosinski2012VisuallyDebuggingRestrictedBoltzmannMachine.pdf
+ * 
+ * 
+ * for visualizations and concepts 
+ * 
+ * @author josh
+ *
+ */
+public class NeuralNetworkVisualizer {
+
+}
