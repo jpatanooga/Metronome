9af3f060f7457bf7d60b7dcc8a3ec77eff456408
Josh Patterson <jpatterson@floe.tv>
2014-02-07 17:49:13 -0500

    re-wrote some of the comments in the RBM reconstruction viz unit test

--- src/test/java/tv/floe/metronome/deeplearning/rbm/datasets/mnist/TestMNIST_On_RBMs.java
+++ src/test/java/tv/floe/metronome/deeplearning/rbm/datasets/mnist/TestMNIST_On_RBMs.java
@@ -19,11 +19,42 @@ public class TestMNIST_On_RBMs {
 
 	private static Logger log = LoggerFactory.getLogger(TestMNIST_On_RBMs.class);
 
+	
+	private void renderExample( Matrix draw1, Matrix reconstructed2, Matrix draw2 ) throws InterruptedException {
+		
+/*
+		Matrix draw1 = first.get(j).getFirst().times(255);
+		
+		//Matrix reconstructed2 = reconstruct.getRow(j);
+		Matrix reconstructed2 = MatrixUtils.viewRowAsMatrix(reconstruct, j);
+		
+		//Matrix draw2 = MatrixUtils.binomial(reconstructed2,1,new MersenneTwister(123)).mul(255);
+		Matrix draw2 = MatrixUtils.genBinomialDistribution(reconstructed2,1,new MersenneTwister(123)).times(255);
+*/
+		DrawMnistGreyscale d = new DrawMnistGreyscale(draw1);
+		d.title = "REAL";
+		d.draw();
+		d.frame.setLocation(100, 200);
+		
+		DrawMnistGreyscale d2 = new DrawMnistGreyscale( draw2, 100, 100 );
+		d2.title = "TEST";
+		d2.draw();
+		d2.frame.setLocation(300, 200);
+
+		Thread.sleep(1000);
+		d.frame.dispose();
+		d2.frame.dispose();
+		
+		
+		
+	}
+	
 	@Test
 	public void testMnist() throws Exception {
 		MnistDataSetIterator fetcher = new MnistDataSetIterator(100,100);
 		MersenneTwister rand = new MersenneTwister(123);
 
+		double learningRate = 0.001;
 		
 		DataSet first = fetcher.next();
 /*
@@ -40,37 +71,47 @@ public class TestMNIST_On_RBMs {
 		rbm.trainingDataset = first.getFirst();
 
 		System.out.println(" ----- Training ------");
-		for(int i = 0; i < 20; i++) {
+		
+		for(int i = 0; i < 10; i++) {
 			
-			rbm.trainTillConvergence( first.getFirst(), 0.01, new Object[]{ 1 } );
+			System.out.println("Epoch " + i + " Negative Log Likelhood: " + rbm.getReConstructionCrossEntropy() );
+			
+			//rbm.trainTillConvergence( first.getFirst(), learningRate, new Object[]{ 1 } );
+			rbm.trainTillConvergence(learningRate, 1, first.getFirst());
+			
 			
 		}
 		
 		Matrix reconstruct = rbm.reconstruct(first.getFirst());
 
+		log.info("Negative log likelihood " + rbm.getReConstructionCrossEntropy());
+
 		System.out.println(" ----- Visualizing Reconstructions ------");
 		
-		for (int j = 0; j < first.numExamples(); j++) {
+		for (int j = 0; j < 10; j++) {
 			
+			// get the actual image we're looking at
 			Matrix draw1 = first.get(j).getFirst().times(255);
 			
-			//Matrix reconstructed2 = reconstruct.getRow(j);
+			// get the reconstruction row that matches this image
 			Matrix reconstructed2 = MatrixUtils.viewRowAsMatrix(reconstruct, j);
 			
-			//Matrix draw2 = MatrixUtils.binomial(reconstructed2,1,new MersenneTwister(123)).mul(255);
+			// now generate a new image based on the reconstruction probabilities
 			Matrix draw2 = MatrixUtils.genBinomialDistribution(reconstructed2,1,new MersenneTwister(123)).times(255);
 
+			// draw the original image
 			DrawMnistGreyscale d = new DrawMnistGreyscale(draw1);
 			d.title = "REAL";
 			d.draw();
 			d.frame.setLocation(100, 200);
 			
+			// now draw the reconstructed image
 			DrawMnistGreyscale d2 = new DrawMnistGreyscale( draw2, 100, 100 );
 			d2.title = "TEST";
 			d2.draw();
 			d2.frame.setLocation(300, 200);
 
-			Thread.sleep(1000);
+			Thread.sleep(5000);
 			d.frame.dispose();
 			d2.frame.dispose();
 			
@@ -78,15 +119,17 @@ public class TestMNIST_On_RBMs {
 
 		}
 		
+		
 		for (int i = 0; i < 3000; i++) {
 			
-			if (i% 500 == 0 || i == 0) {
+			if (i% 100 == 0 || i == 0) {
 				
 				reconstruct = rbm.reconstruct(first.getFirst());
 				
 				if (i > 0)
 					
-					for (int j = 0; j < first.numExamples(); j++) {
+					//for (int j = 0; j < first.numExamples(); j++) {
+					for (int j = 0; j < 10; j++) {
 						
 
 						
@@ -101,7 +144,7 @@ public class TestMNIST_On_RBMs {
 						DrawMnistGreyscale d2 = new DrawMnistGreyscale(draw2,100,100);
 						d2.title = "TEST";
 						d2.draw();
-						Thread.sleep(1000);
+						Thread.sleep(5000);
 						d.frame.dispose();
 						d2.frame.dispose();
 
@@ -110,7 +153,7 @@ public class TestMNIST_On_RBMs {
 			
 			rbm.train(first.getFirst(), 0.01, new Object[]{1});
 			
-			log.info("Negative log likelihood " + rbm.getReConstructionCrossEntropy());
+			log.info("Training Epoch " + i + " Negative log likelihood " + rbm.getReConstructionCrossEntropy());
 
 
 		}
