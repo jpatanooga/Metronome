2e4e002600da3fa25bd7dd530f9486435da90f9b
Josh Patterson <jpatterson@floe.tv>
2014-02-15 09:49:46 -0500

    added base serde methods for LogisticRegressionGradient

--- src/main/java/tv/floe/metronome/deeplearning/neuralnetwork/core/LogisticRegressionGradient.java
+++ src/main/java/tv/floe/metronome/deeplearning/neuralnetwork/core/LogisticRegressionGradient.java
@@ -1,8 +1,17 @@
 package tv.floe.metronome.deeplearning.neuralnetwork.core;
 
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
 
 import org.apache.mahout.math.Matrix;
+import org.apache.mahout.math.MatrixWritable;
 
 import tv.floe.metronome.math.MatrixUtils;
 
@@ -44,23 +53,66 @@ public class LogisticRegressionGradient implements Serializable {
 			return false;
 		return true;
 	}
+	
 	public LogisticRegressionGradient(Matrix wGradient,
 			Matrix bGradient) {
 		super();
 		this.wGradient = wGradient;
 		this.bGradient = bGradient;
 	}
+	
 	public Matrix getwGradient() {
 		return wGradient;
 	}
+	
 	public void setwGradient(Matrix wGradient) {
 		this.wGradient = wGradient;
 	}
+	
 	public Matrix getbGradient() {
 		//return bGradient.columnMeans();
 		return MatrixUtils.columnMeans(bGradient);
 	}
+	
 	public void setbGradient(Matrix bGradient) {
 		this.bGradient = bGradient;
 	}
+	
+	/**
+	 * Serializes this to the output stream.
+	 * @param os the output stream to write to
+	 */
+	public void write(OutputStream os) {
+		try {
+
+		    DataOutput d = new DataOutputStream(os);
+			
+		    MatrixWritable.writeMatrix(d, this.bGradient );
+		    MatrixWritable.writeMatrix(d, this.wGradient );			
+		    
+
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+	}	
+	
+	/**
+	 * Load (using {@link ObjectInputStream}
+	 * @param is the input stream to load from (usually a file)
+	 */
+	public void load(InputStream is) {
+		try {
+
+			DataInput di = new DataInputStream(is);
+
+			this.bGradient = MatrixWritable.readMatrix( di );
+			this.wGradient = MatrixWritable.readMatrix( di );
+			
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+	}		
+	
 }
