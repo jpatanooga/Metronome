9c896a8deea5bf6d0ce313becbfaf38463e8b93c
Josh Patterson <jpatterson@floe.tv>
2014-02-15 09:31:31 -0500

    rebuilt the Hidden Layer unit test for serde

--- src/test/java/tv/floe/metronome/deeplearning/neuralnetwork/layer/TestHiddenLayer.java
+++ src/test/java/tv/floe/metronome/deeplearning/neuralnetwork/layer/TestHiddenLayer.java
@@ -2,6 +2,10 @@ package tv.floe.metronome.deeplearning.neuralnetwork.layer;
 
 import static org.junit.Assert.*;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 import org.apache.commons.math3.random.MersenneTwister;
 import org.apache.commons.math3.random.RandomGenerator;
 import org.apache.mahout.math.DenseMatrix;
@@ -13,22 +17,24 @@ import tv.floe.metronome.math.MatrixUtils;
 
 public class TestHiddenLayer {
 
+	Matrix input = new DenseMatrix(new double[][] 
+	{
+			{1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0}
+			,{1,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0}
+			,{1,1,0,1,1,1,1,1,0,1,0,0,0,0,0,0,0,0,0,0}
+			,{1,1,1,1,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0}
+			,{0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0}
+			,{0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1}
+			,{0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,1,1,1,1}
+			,{0,0,0,0,0,0,0,0,0,0,1,1,0,1,1,1,1,1,0,1}
+			,{0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,1,1,1,1}
+			,{0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0}
+    }
+	);	
+	
 	@Test
 	public void testSimple() {
-		Matrix input = new DenseMatrix(new double[][] 
-		{
-				{1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0}
-				,{1,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0}
-				,{1,1,0,1,1,1,1,1,0,1,0,0,0,0,0,0,0,0,0,0}
-				,{1,1,1,1,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0}
-				,{0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0}
-				,{0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1}
-				,{0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,1,1,1,1}
-				,{0,0,0,0,0,0,0,0,0,0,1,1,0,1,1,1,1,1,0,1}
-				,{0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,1,1,1,1}
-				,{0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0}
-        }
-		);
+
 		
 		MatrixUtils.debug_print_matrix_stats( input, "input" );
 		
@@ -94,5 +100,40 @@ public class TestHiddenLayer {
 	public void testSampleHiddenGivenLastVisible() {
 		
 	}
+	
+
+	@Test
+	public void testSerdeMechanics() throws FileNotFoundException {
+		
+		String tmpFilename = "/tmp/hiddenLayerTest.model";
+		
+		HiddenLayer layer = new HiddenLayer(20, 2, null);
+		layer.setInput(input);
+		
+		
+		
+		// save / write the model
+		
+		FileOutputStream oFileOutStream = new FileOutputStream( tmpFilename, false);
+		layer.write( oFileOutStream );
+		
+		
+		
+		
+		// read / load the model
+		FileInputStream oFileInputStream = new FileInputStream( tmpFilename );
+		
+		HiddenLayer layer_deser = new HiddenLayer( 1, 1, null); 
+		layer_deser.load(oFileInputStream);
+		
+		assertEquals( layer.neuronCount, layer_deser.neuronCount );
+		assertEquals( layer.neuronCountPreviousLayer, layer_deser.neuronCountPreviousLayer );
+		
+
+		assertEquals( true, MatrixUtils.elementwiseSame(layer.biasTerms, layer_deser.biasTerms ) );
+		assertEquals( true, MatrixUtils.elementwiseSame(layer.connectionWeights, layer_deser.connectionWeights ) );		
+		assertEquals( true, MatrixUtils.elementwiseSame(layer.input, layer_deser.input ) );
+		
+	}		
 
 }
