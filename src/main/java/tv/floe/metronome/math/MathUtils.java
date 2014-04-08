b4ea3f475d5c332e85746e065d677dfd85d5b225
Josh Patterson <jpatterson@floe.tv>
2014-04-07 22:37:05 -0400

    added a rdot() method to MathUtils for scalar dot product

--- src/main/java/tv/floe/metronome/math/MathUtils.java
+++ src/main/java/tv/floe/metronome/math/MathUtils.java
@@ -49,5 +49,23 @@ public class MathUtils {
 	    return (double) Math.sqrt(norm);
 	    
 	}	
+	
+	/** Compute scalar product between dx and dy. 
+	 *
+	 * Source: JBLAS.rdot() impl
+	 */
+	public static double rdot(int n, double[] dx, int dxIdx, int incx, double[] dy, int dyIdx, int incy) {
+        double s = 0.0;
+        if (incx == 1 && incy == 1 && dxIdx == 0 && dyIdx == 0) {
+            for (int i = 0; i < n; i++)
+                s += dx[i] * dy[i];
+        }
+        else {
+            for (int c = 0, xi = dxIdx, yi = dyIdx; c < n; c++, xi += incx, yi += incy) {
+                s += dx[xi] * dy[yi];
+            }
+        }
+        return s;
+    }	
 
 }
