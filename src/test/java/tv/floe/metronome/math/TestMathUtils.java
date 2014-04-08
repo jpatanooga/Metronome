da41ac564de3871b911a7a30ba34b13a7d55d138
Josh Patterson <jpatterson@floe.tv>
2014-04-07 21:50:26 -0400

    added a unit test for MathUtils.norm2()

--- src/test/java/tv/floe/metronome/math/TestMathUtils.java
+++ src/test/java/tv/floe/metronome/math/TestMathUtils.java
@@ -28,4 +28,26 @@ public class TestMathUtils {
 		
 	}
 
+	
+	@Test
+	public void testNorm2() {
+		
+		Matrix m = new DenseMatrix(1, 2);
+		m.setQuick(0, 0, 2);
+		m.setQuick(0, 1, 4);
+
+		Matrix m2 = new DenseMatrix(1, 2);
+		m2.setQuick(0, 0, -2);
+		m2.setQuick(0, 1, 2);
+
+		double m_ret = MathUtils.norm2(m);
+		double m2_ret = MathUtils.norm2(m2);
+		
+		assertEquals( Math.sqrt(20), m_ret, 0.0 );
+		assertEquals( Math.sqrt(8), m2_ret, 0.0 );
+		
+		
+	}	
+	
+	
 }
