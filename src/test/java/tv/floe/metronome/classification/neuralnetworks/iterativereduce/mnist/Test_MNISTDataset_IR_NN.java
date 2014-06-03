c2be173e275892cc79b71fff68d0b9e4d32d088a
Josh Patterson <jpatterson@floe.tv>
2014-06-02 18:06:29 -0700

    quick cleanup

--- src/test/java/tv/floe/metronome/classification/neuralnetworks/iterativereduce/mnist/Test_MNISTDataset_IR_NN.java
+++ src/test/java/tv/floe/metronome/classification/neuralnetworks/iterativereduce/mnist/Test_MNISTDataset_IR_NN.java
@@ -12,12 +12,12 @@ public class Test_MNISTDataset_IR_NN {
 
 	@Test
 	public void testLearnIrisFunctionViaIRNN_MLP() throws Exception {
-		
+		/*
 		IRUnitDriver polr_ir = new IRUnitDriver("src/test/resources/run_profiles/unit_tests/nn/mnist/app.unit_test.nn.mnist.properties");
 		polr_ir.Setup();
 
 		polr_ir.SimulateRun();
-
+*/
 		
 		
 		
